e1c0e07de43fba9e4012ac837eb4f5ffc204a5c2
anomaddev <anomaddev@gmail.com>
2024-12-02 17:44:20 -0500

    working

--- distillery/core.py
+++ distillery/core.py
@@ -7,9 +7,28 @@
 #
 #
 
+# I2C Devices
 from RPLCD.i2c import CharLCD # type: ignore
+
 from display import *
+from temperature import *
+
+from threading import Thread
+
+import time
 
 version = "0.0.1"
 
+# Show initial screen for at least 5 seconds
 start_screen(version)
+time.sleep(1)
+
+# Set initial values
+update_screen("--", "--", "STARTUP", "OFF", "OFF")
+
+t1 = Thread(target = start_reading_dial)
+t2 = Thread(target = run_heating)
+
+print("Starting threads..")
+t1.start()
+t2.start()
