c6a29ba6c928afde123e0ea28a99999e1258255e
anomaddev <anomaddev@gmail.com>
2024-12-11 13:37:29 -0500

    working

--- distillery/src/sandbox.py
+++ distillery/src/sandbox.py
@@ -8,6 +8,15 @@
 #
 
 import subprocess
+import board
+import adafruit_tca9548a
 
-def test():
-    print("Running tests..")
+i2c = board.I2C()
+adafruit_tca9548a.TCA9548A(i2c)
+
+for channel in range(8):
+    if tca[channel].try_lock():
+        print("Channel {} locked".format(channel))
+        addresses = tca[channel].scan()
+        print([hex(address) for address in addresses if address != 0x70])
+        tca[channel].unlock()
