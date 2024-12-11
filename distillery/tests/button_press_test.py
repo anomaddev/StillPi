213704d2c88b7e879a9782c975fa758aa0b4c91c
anomaddev <anomaddev@gmail.com>
2024-12-10 22:48:29 -0500

    working

--- distillery/tests/button_press_test.py
+++ distillery/tests/button_press_test.py
@@ -5,11 +5,11 @@ button_pin = 6
 
 # Set GPIO mode
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(button_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(button_pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
 def button_press(button):
     print("Button pressed!")
-    if GPIO.input(button) == GPIO.LOW:
+    if GPIO.input(button) == GPIO.HIGH:
         print("Button depressed!")
     else:
         print("Button released!")
