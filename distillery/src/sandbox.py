872e93a8a4d72aa297bf7e932050b67f96cab653
anomaddev <anomaddev@gmail.com>
2024-12-11 16:46:08 -0500

    working

--- distillery/src/sandbox.py
+++ distillery/src/sandbox.py
@@ -7,27 +7,10 @@
 #
 #
 
-import subprocess
-import smbus2
 import board
-import adafruit_tca9548a
+import digitalio
+import adafruit_max31865
 
-import time
-
-from RPLCD.i2c import CharLCD # type: ignore
-
-from i2c_devices import *
-
-bus = smbus2.SMBus(1)
-enable_channels(bus, [2, 7])
-
-# for channel in range(8):
-#     if tca[channel].try_lock():
-#         print("Channel {} locked".format(channel))
-#         addresses = tca[channel].scan()
-#         print([hex(address) for address in addresses if address != 0x70])
-#         tca[channel].unlock()
-
-
-# lcd = CharLCD(i2c_expander='PCF8574', address=0x27, port=1, cols=20, rows=4, dotsize=8, backlight_enabled=True)
-# lcd.write_string("Hello, world!")
+spi = board.SPI()
+cs = digitalio.DigitalInOut(board.D5)  # Chip select of the MAX31865 board.
+sensor = adafruit_max31865.MAX31865(spi, cs, wires=3)
