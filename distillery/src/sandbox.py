c067d08b946d62d838a325cd1ea6cbe6a360fced
anomaddev <anomaddev@gmail.com>
2024-12-11 14:28:10 -0500

    working

--- distillery/src/sandbox.py
+++ distillery/src/sandbox.py
@@ -8,6 +8,7 @@
 #
 
 import subprocess
+import smbus2
 import board
 import adafruit_tca9548a
 
@@ -21,13 +22,24 @@ i2c = board.I2C()
 # Create the TCA9548A object and give it the I2C bus
 tca = adafruit_tca9548a.TCA9548A(i2c)
 
-for channel in range(8):
-    if tca[channel].try_lock():
-        print("Channel {} locked".format(channel))
-        addresses = tca[channel].scan()
-        print([hex(address) for address in addresses if address != 0x70])
-        tca[channel].unlock()
+TCA_ADDR = 0x70
+
+def select_channel(bus, channel):
+    if 0 <= channel <= 7:
+        bus.write_byte(TCA_ADDR, 1 << channel)
+    else:
+        raise ValueError("Channel must be in range 0-7")
+    
+bus = smbus2.SMBus(1)
+select_channel(bus, 2)
+
+# for channel in range(8):
+#     if tca[channel].try_lock():
+#         print("Channel {} locked".format(channel))
+#         addresses = tca[channel].scan()
+#         print([hex(address) for address in addresses if address != 0x70])
+#         tca[channel].unlock()
 
 
-lcd = CharLCD(i2c_expander='PCF8574', address=tca[2], port=1, cols=20, rows=4, dotsize=8, backlight_enabled=True)
+lcd = CharLCD(i2c_expander='PCF8574', address=0x27, port=1, cols=20, rows=4, dotsize=8, backlight_enabled=True)
 lcd.write_string("Hello, world!")
