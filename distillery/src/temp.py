210c731e7535bc4fe0d6a803c185a33a2923fa92
anomaddev <anomaddev@gmail.com>
2024-12-11 02:40:11 -0500

    working

--- distillery/src/temp.py
+++ distillery/src/temp.py
@@ -5,20 +5,3 @@
 #
 #
 
-from MAX6675 import MAX6675
-
-cs = 22
-sck = 18
-so = 16
-
-max = MAX6675.MAX6675(cs, sck, so)
-
-try:
-    while 1:
-        a = MAX6675.read_temp(cs)
-        print(a)
-
-        MAX6675.time.sleep(2)
-
-except KeyboardInterrupt:
-    pass
