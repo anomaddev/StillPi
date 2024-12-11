2b63e8a26e502a89dfff461e825c5119e146e4e7
anomaddev <anomaddev@gmail.com>
2024-12-11 03:35:38 -0500

    working

--- distillery/core.py
+++ distillery/core.py
@@ -18,36 +18,36 @@ from src.relays import *
 from src.display import *
 from src.interface import *
 
-initial_load = True
 version = os.environ['DISTILLERY_VERSION']
 
 def core_function():
-    global initial_load
     print("Beginning core functionality..")
     print("Version: " + version)
     print()
 
-    if initial_load:
-        initial_load = False
+    # Set GPIO mode
+    GPIO.setmode(GPIO.BCM)
+    
+    # Setup the relays
+    setup_relays()
+    show_text_on_line(3, "Heater SSRs Ready")
+    time.sleep(2.5)
+
+    setup_interface()
+    show_text_on_line(3, "Buttons Active")
+    time.sleep(2.5)
+
+    # Initialize the screen
+    init_screen()
+    time.sleep(2.5)
+
+    # Get initial temperature reading
+    stabilize_temp()
+
+    # Await start button press
+    start_button_await()
         
-        # Set GPIO mode
-        GPIO.setmode(GPIO.BCM)
         
-        # Setup the relays
-        setup_relays()
-        show_text_on_line(3, "Heater SSRs Ready")
-        time.sleep(2.5)
-
-        setup_interface()
-        show_text_on_line(3, "Buttons Active")
-        time.sleep(2.5)
-
-        # Initialize the screen
-        init_screen()
-        time.sleep(2.5)
-
-        # Get initial temperature reading
-        stabilize_temp()
 
 def stabilize_temp():
     print("Stabilizing temperature..")
--- distillery/src/interface.py
+++ distillery/src/interface.py
@@ -13,15 +13,34 @@ from core import *
 
 start_button = 26
 
+start_depressed = False
+start_toggle = False
+
 def start_pressed(button):
+    global start_toggle
+    global start_depressed
+
     if GPIO.input(button) == GPIO.HIGH:
         print("Button depressed!")
+        start_depressed = True
     else:
         print("Button released!")
+        if start_depressed:
+            start_toggle = True
+            start_depressed = False
 
 def setup_interface():
     print("Setting up interfaces..")
 
     GPIO.setup(start_button, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
     GPIO.add_event_detect(start_button, GPIO.BOTH, callback=start_pressed, bouncetime=10)
+
+def start_button_await():
+    while not start_toggle:
+        pass
+
+    print("start button pressed")
+    GPIO.remove_event_detect(start_button)
+    return
+
     
