5990942d4a00d46fdca0909cb74368af77b39bfd
anomaddev <anomaddev@gmail.com>
2024-12-11 01:49:08 -0500

    working

--- distillery/src/temp.py
+++ distillery/src/temp.py
@@ -5,3 +5,20 @@
 #
 #
 
+import max6675
+
+cs = 22
+sck = 18
+so = 16
+
+max = max6675.set_pin(cs, sck, so, 2)
+
+try:
+    while 1:
+        a = max6675.read_temp(cs)
+        print(a)
+
+        max6675.time.sleep(2)
+        
+except KeyboardInterrupt:
+    pass
