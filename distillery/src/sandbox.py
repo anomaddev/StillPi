afa46721d0ba4d6061a13e5c9f8ac5b1dc37bf0b
anomaddev <anomaddev@gmail.com>
2024-12-12 00:48:50 -0500

    working

--- distillery/src/sandbox.py
+++ distillery/src/sandbox.py
@@ -15,7 +15,6 @@ spi = board.SPI()
 cs = digitalio.DigitalInOut(board.D5)  # Chip select of the MAX31865 board.
 sensor = adafruit_max31865.MAX31865(spi, cs)
 
-print('Temperature: {0:0.3f}C'.format(sensor.temperature))
-print(sensor.temperature)
-print()
+fTemp = sensor.temperature * 9 / 5 + 32
+print('Temperature: {0:0.3f}F'.format(fTemp))
 print('Resistance: {0:0.3f} Ohms'.format(sensor.resistance))
